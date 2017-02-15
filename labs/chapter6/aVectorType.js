02d46d83eade0891a4fc1d1d3b06e691130faa44
Monks <thaiattt@gmail.com>
2017-02-15 11:29:35 +0700

    chapter6

---
+++ labs/chapter6/aVectorType.js
@@ -0,0 +1,28 @@
+/**
+ * Created by thaidl on 15-02-2017.
+ */
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function (vector_other) {
+    return new Vector(this.x + vector_other.x, this.y + vector_other.y );
+};
+
+Vector.prototype.minus = function (vecter_other) {
+    return new Vector(this.x - vecter_other.x, this.y - vecter_other.y);
+};
+
+Object.defineProperty(Vector.prototype, "length",{
+    get: function() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+});
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
---
+++ labs/chapter6/script.js
@@ -0,0 +1,278 @@
+/**
+ * Created by thaidl on 15-02-2017.
+ */
+//Methods
+var rabbit = {};
+rabbit.speak = function(line) {
+  console.log("The rabbit says '" + line + "'");
+};
+rabbit.speak("I'm alive.");
+
+function speak(line) {
+  console.log("The " + this.type + " rabbit says '" + line + "'");
+}
+var whiteRabbit = {type: "white", speak: speak};
+var fatRabbit = {type: "fat", speak: speak};
+
+whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
+fatRabbit.speak("I could sure use a carrot right now.");
+
+//Prototypes
+var empty = {};
+console.log(empty.toString);
+console.log(empty.toString());
+
+//- Get prototype of an object
+console.log(Object.getPrototypeOf({}) == Object.prototype);
+console.log(Object.getPrototypeOf(Object.prototype));
+
+//- Object.create to create an object with a specific prototype
+var protoRabbit = {
+  speak: function(line) {
+    console.log("The " + this.type + " rabbit says '" +         line + "'");
+  }
+};
+var killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "killer";
+killerRabbit.speak("SKREEEE!");
+
+//Constructors
+function Rabbit(type) {
+  this.type = type;
+}
+
+var killerRabbit = new Rabbit("killer");
+var blackRabbit = new Rabbit("black");
+console.log(blackRabbit.type);
+
+Rabbit.prototype.speak = function(line) {
+  console.log("The " + this.type + " rabbit says '" + line + "'");
+};
+blackRabbit.speak("Doom...");
+
+
+//Overriding derived properties
+Rabbit.prototype.teeth = "small";
+console.log(killerRabbit.teeth);
+killerRabbit.teeth = "long, sharp, and bloody";
+console.log(killerRabbit.teeth);
+console.log(blackRabbit.teeth);
+console.log(Rabbit.prototype.teeth);
+
+console.log(Array.prototype.toString == Object.prototype.toString);
+console.log([1, 2].toString());
+
+//-
+console.log(Array.prototype.toString == Object.prototype.toString);
+// → false
+console.log([1, 2].toString());
+// → 1,2
+console.log(Object.prototype.toString.call([1, 2]));
+
+//Prototype interference
+Rabbit.prototype.dance = function() {
+  console.log("The " + this.type + " rabbit dances a jig.");
+};
+killerRabbit.dance();
+
+//-
+var map = {};
+function storePhi(event, phi) {
+  map[event] = phi;
+}
+
+storePhi("pizza", 0.069);
+storePhi("touched tree", -0.081);
+console.log(map);
+
+//-
+Object.prototype.nonsense = "hi";
+for (var name in map)
+  console.log(name);
+
+console.log("nonsense" in map);
+console.log("toString" in map);
+
+// Delete the problematic property again
+delete Object.prototype.nonsense;
+
+//--
+Object.defineProperty(Object.prototype, "hiddenNonsense", {enumerable: false, value: "hi"});
+for (var name in map)
+  console.log(name);
+
+console.log(map.hiddenNonsense);
+console.log("toString" in map);
+console.log(map.hasOwnProperty("toString"));
+
+
+//Prototype-less objects
+var map = Object.create(null);
+map["pizza"] = 0.069;
+console.log("toString" in map);// → false
+console.log("pizza" in map);// → true
+
+
+//Polymorphism
+
+//Laying out a table
+console.log("----------------------------\n");
+function rowHeights(rows) {
+  return rows.map(function(row) {
+    return row.reduce(function(max, cell) {
+      return Math.max(max, cell.minHeight());
+    }, 0);
+  });
+}
+
+function colWidths(rows) {
+  return rows[0].map(function(_, i) {
+    return rows.reduce(function(max, row) {
+      return Math.max(max, row[i].minWidth());
+    }, 0);
+  });
+}
+
+function drawTable(rows) {
+  var heights = rowHeights(rows);
+  var widths = colWidths(rows);
+
+  function drawLine(blocks, lineNo) {
+    return blocks.map(function(block) {
+      return block[lineNo];
+    }).join(" ");
+  }
+
+  function drawRow(row, rowNum) {
+    var blocks = row.map(function(cell, colNum) {
+      return cell.draw(widths[colNum], heights[rowNum]);
+    });
+    return blocks[0].map(function(_, lineNo) {
+      return drawLine(blocks, lineNo);
+    }).join("\n");
+  }
+
+  return rows.map(drawRow).join("\n");
+}
+
+function repeat(string, times) {
+  var result = "";
+  for (var i = 0; i < times; i++)
+    result += string;
+  return result;
+}
+
+function TextCell(text) {
+  this.text = text.split("\n");
+}
+TextCell.prototype.minWidth = function() {
+  return this.text.reduce(function(width, line) {
+    return Math.max(width, line.length);
+  }, 0);
+};
+TextCell.prototype.minHeight = function() {
+  return this.text.length;
+};
+TextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(line + repeat(" ", width - line.length));
+  }
+  return result;
+};
+
+var rows = [];
+for (var i = 0; i < 5; i++) {
+   var row = [];
+   for (var j = 0; j < 5; j++) {
+     if ((j + i) % 2 == 0)
+       row.push(new TextCell("##"));
+     else
+       row.push(new TextCell("  "));
+   }
+   rows.push(row);
+}
+console.log(drawTable(rows));
+
+//-- Mountains
+var MOUNTAINS = [
+  {name: "Kilimanjaro", height: 5895, country: "Tanzania"},
+  {name: "Everest", height: 8848, country: "Nepal"},
+  {name: "Mount Fuji", height: 3776, country: "Japan"},
+  {name: "Mont Blanc", height: 4808, country: "Italy/France"},
+  {name: "Vaalserberg", height: 323, country: "Netherlands"},
+  {name: "Denali", height: 6168, country: "United States"},
+  {name: "Popocatepetl", height: 5465, country: "Mexico"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = MOUNTAINS;
+
+function UnderlinedCell(inner) {
+  this.inner = inner;
+}
+UnderlinedCell.prototype.minWidth = function() {
+  return this.inner.minWidth();
+};
+UnderlinedCell.prototype.minHeight = function() {
+  return this.inner.minHeight() + 1;
+};
+UnderlinedCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height - 1)
+    .concat([repeat("-", width)]);
+};
+
+function dataTable(data) {
+  var keys = Object.keys(data[0]);
+  var headers = keys.map(function(name) {
+    return new UnderlinedCell(new TextCell(name));
+  });
+  var body = data.map(function(row) {
+    return keys.map(function(name) {
+      return new TextCell(String(row[name]));
+    });
+  });
+  return [headers].concat(body);
+}
+
+console.log(drawTable(dataTable(MOUNTAINS)));
+
+
+//Getters and setters
+var pile = {
+  elements: ["eggshell", "orange peel", "worm"],
+  get height() {
+    return this.elements.length;
+  },
+  set height(value) {
+    console.log("Ignoring attempt to set height to", value);
+  }
+};
+
+console.log(pile.height);
+pile.height = 100;
+console.log(pile.height);
+
+Object.defineProperty(TextCell.prototype, "heightProp", {
+  get: function() { return this.text.length; }
+});
+
+var cell = new TextCell("no\nway");
+console.log(cell.heightProp);
+cell.heightProp = 100;
+console.log(cell.heightProp);
+
+//Inheritance
+function RTextCell(text) {
+  TextCell.call(this, text);
+}
+RTextCell.prototype = Object.create(TextCell.prototype);
+RTextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(repeat(" ", width - line.length) + line);
+  }
+  return result;
+};
